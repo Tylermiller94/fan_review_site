4a335524fff5d231a5bbfb31e910771876c514cc
Guest User <Guest@epicodus-2.local>
2017-11-29 19:53:35 -0800

    add destroy to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,7 +35,13 @@ class MoviesController < ApplicationController
       render :edit
     end
   end
-  
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path
+  end
+
   private
   def movie_params
     params.require(:movie).permit(:name, :release, :description)
